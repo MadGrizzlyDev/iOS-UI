10fd9b56dc0b8d445720ff874e3f9bcfda1f0c28
MadGrizzlyDev <pgbelov89@gmail.com>
2021-05-05 14:49:24 +0300

    new project

---
+++ Netology_IB_Instruments/Netology_IB_Instruments/ProfileView.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileView.swift
+//  Netology_IB_Instruments
+//
+//  Created by Pavel Belov on 05.05.2021.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+    @IBOutlet weak var avatar: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var birthdayDate: UILabel!
+    @IBOutlet weak var location: UILabel!
+    @IBOutlet weak var someText: UITextView!
+    
+}
