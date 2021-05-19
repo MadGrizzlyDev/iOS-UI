06ffc97e761b937fce519af9ddd029b7d21abd10
MadGrizzlyDev <pgbelov89@gmail.com>
2021-05-19 20:11:27 +0300

    HW_2.2

---
+++ Navigation 2.2/Navigation/Profile/ProfileViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ProfileViewController.swift
+//  Navigation
+//
+//  Created by Pavel Belov on 14.05.2021.
+//  Copyright © 2021 Artem Novichkov. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    private let profileHeaderView = ProfileHeaderView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .lightGray
+        view.addSubview(profileHeaderView)
+        // Do any additional setup after loading the view.
+    }
+
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        profileHeaderView.frame = view.frame
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+   }
+}
+//.init(x: 1, y: 1, width: 1, height: 1)
