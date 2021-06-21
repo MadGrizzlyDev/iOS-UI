7d75f5147329b2e147dc7a7cb9a50fb31ba5119d
MadGrizzlyDev <pgbelov89@gmail.com>
2021-06-21 23:56:59 +0300

    done

---
+++ Navigation 2.3 part2/Navigation/Profile/PostFeed.swift
@@ -0,0 +1,18 @@
+//
+//  PostFeed.swift
+//  Navigation
+//
+//  Created by Pavel Belov on 18.06.2021.
+//  Copyright © 2021 Artem Novichkov. All rights reserved.
+//
+
+import UIKit
+
+struct PostFeed {
+    let author: String
+    let description: String
+    let image: String
+    let likes: Int
+    let views: Int
+}
+
